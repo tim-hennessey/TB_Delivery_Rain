0a55cc8f74f73d09b8d356212cd0c5703867b842
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-01-24 14:23:58 -0800

    Resizes

---
+++ app/320x50/scripts/Animation.js
@@ -0,0 +1,103 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+    var t = TweenMax;
+    var tl1 = new TimelineMax({repeat: -1, paused: true});
+    var pintl = new TimelineMax();
+    var tltxt = new TimelineMax();
+
+    var pin = document.getElementById('pin');
+    var txt1 = document.getElementById('txt1');
+    var txt2 = document.getElementById('txt2');
+    var txt3 = document.getElementById('txt3');
+    var buttonExit = document.getElementById('button-exit');
+
+    // --------------------------------------------------------------------------------------
+    // set default properties
+    function initialize() {
+
+
+        t.set(banner, {opacity: 1});
+        t.set("#container", {perspective: 600});
+        t.set(pin, {y:"-=10", transformOrigin: "50% 100%"});
+
+        buttonExit.addEventListener('mouseover', function () {
+
+        });
+        buttonExit.addEventListener('mouseout', function () {
+
+        });
+
+    }
+
+    // --------------------------------------------------------------------------------------
+    // Starts the animation
+    function start() {
+
+        var total = 300;
+        var container = document.getElementById("container"), w = window.innerWidth, h = window.innerHeight;
+
+        for (var i = 0; i < total; i++) {
+            var Div = document.createElement('div');
+            t.set(Div, {
+                attr: {class: 'dot'},
+                x: R(0, w),
+                y: R(-100, -150),
+                scaleY: R(.01, 1),
+                opacity: R(.25, .75)
+            });
+            container.appendChild(Div);
+            animm(Div);
+        }
+
+        function animm(elm) {
+            t.to(elm, R(1, 2), {y: h - 50, ease: Linear.easeNone, repeat: -1, delay: -5});
+        }
+
+        function R(min, max) {
+            return min + Math.random() * (max - min)
+        }
+
+        pintl.from(pin, .25, {scale:"-=.2", opacity:0, ease: Sine.easeInOut}, "+=3.5")
+            .to(pin, .25, {y:"+=10", ease: Sine.easeIn}, "+=.2")
+            .to(pin, .25, {y:"-=5", ease: Sine.easeOut})
+            .to(pin, .25, {y:"+=5", ease: Sine.easeIn})
+            .to(pin, 1, {y: "-=5", ease: Sine.easeOut, onComplete: function () {tl1.play()}});
+
+
+
+        tltxt.to(txt1, .25, {opacity: 1}, "+=.75")
+            .fromTo(txt1, .25, {y: "-=20"}, {y: "+=23", ease: Sine.easeOut}, "-=.25")
+            .to(txt1, .1, {y: "-=3", ease: Sine.easeInOut})
+
+            .to(txt2, .25, {opacity: 1}, "+=.75")
+            .fromTo(txt2, .25, {y: "-=20"}, {y: "+=23", ease: Sine.easeOut}, "-=.25")
+            .to(txt2, .1, {y: "-=3", ease: Sine.easeInOut})
+
+            .to(txt3, .25, {opacity: 1}, "+=1");
+
+
+        tl1.to(pin, 1, {y: '+=5', ease: Sine.easeInOut})
+            .to(pin, 1, {y: '-=5', ease: Sine.easeInOut});
+
+
+    }
+
+    // --------------------------------------------------------------------------------------
+    // Stops the animation
+    function stop() {
+        console.log("stopping animation");
+    }
+
+    // --------------------------------------------------------------------------------------
+    // Publicly accessible methods and properties
+    return {
+        initialize: initialize,
+        start: start,
+        stop: stop
+    }
+
+
+})();
